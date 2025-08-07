9843ec1dbd5fc3a87d9317f34cf43d9fd75f2526
SimpleCaci <edikus@outlook.es>
2025-08-07 14:54:43 -0700

    MergeSort.py works!!

--- CS-Fundamentals-Practice/06_Algorithms/Sorting Algorithms/Efficient Comparison-Based Algorithms/mergeSort.py
+++ CS-Fundamentals-Practice/06_Algorithms/Sorting Algorithms/Efficient Comparison-Based Algorithms/mergeSort.py
@@ -1,14 +1,34 @@
-inputList = [22,3,1,4,23,43,5,9,23]
+inputList = [22,3,1,4,23,43,5,9,3,23,24,511,323423,4,23]
 
 #first I split
 #then I sort
 #then I merge
 
-def split(ogList):
-    if (len(ogList)==1):
+def splitAndMerge(ogList):
+    if len(ogList) == 1:
         return ogList
-    left = split(ogList[0:int(len(ogList)/2)])
-    right = split(ogList[int(len(ogList)/2):len(ogList)])
-    return [left, right]
+    
+    mid = len(ogList) // 2
+    leftList = splitAndMerge(ogList[:mid])
+    rightList = splitAndMerge(ogList[mid:])
+    
+    return sort(leftList, rightList)
 
-print(split(inputList))
+def sort(leftList, rightList):
+    sortedList = []
+
+    while leftList and rightList:
+        if leftList[0] < rightList[0]:
+            sortedList.append(leftList.pop(0))
+        else:
+            sortedList.append(rightList.pop(0))
+
+    sortedList += leftList
+    sortedList += rightList
+
+    return sortedList
+
+
+
+
+print(splitAndMerge(inputList))
